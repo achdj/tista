2d944692c26cb37461f80f4cf43b2008d8f4e9c2
achDj <achilledjegnon@gmail.com>
2021-02-16 17:17:19 +0000

    five commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDqzhqTac0GVyzAEEIpeGtxbXc_ZKQEXGo",
+    authDomain: "tibus-6d312.firebaseapp.com",
+    databaseURL: "https://tibus-6d312-default-rtdb.firebaseio.com",
+    projectId: "tibus-6d312",
+    storageBucket: "tibus-6d312.appspot.com",
+    messagingSenderId: "355227239694",
+    appId: "1:355227239694:web:19a55012cc84ac06b0835f"
+
+  }
 };
