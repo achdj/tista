3835681dc12449d5023e7fd4fd8d5bf0cd2651e4
achDj <achilledjegnon@gmail.com>
2021-02-19 18:59:08 +0000

    sevenupdate commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,22 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDqzhqTac0GVyzAEEIpeGtxbXc_ZKQEXGo",
+    /*apiKey: "AIzaSyDqzhqTac0GVyzAEEIpeGtxbXc_ZKQEXGo",
     authDomain: "tibus-6d312.firebaseapp.com",
     databaseURL: "https://tibus-6d312-default-rtdb.firebaseio.com",
     projectId: "tibus-6d312",
     storageBucket: "tibus-6d312.appspot.com",
     messagingSenderId: "355227239694",
-    appId: "1:355227239694:web:19a55012cc84ac06b0835f"
+    appId: "1:355227239694:web:19a55012cc84ac06b0835f"*/
+
+    apiKey: "AIzaSyD01mY7yLaDb3BwyFI7FfyhdUTgSLt3aCA",
+    authDomain: "tibusdatabase.firebaseapp.com",
+    databaseURL: "https://tibusdatabase-default-rtdb.firebaseio.com/",
+    projectId: "tibusdatabase",
+    storageBucket: "tibusdatabase.appspot.com",
+    messagingSenderId: "87008087331",
+    appId: "1:87008087331:web:a612d564b586db6c1c9096"
+
 
   }
 };
